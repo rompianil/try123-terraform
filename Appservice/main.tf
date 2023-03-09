b463acb6b780b62d2b99126ffadd244d942be3b1
Anil <rompi.anil1998@gmail.com>
2023-03-09 15:35:16 +0530

    c3

--- Appservice/main.tf
+++ Appservice/main.tf
@@ -11,6 +11,23 @@ resource "azurerm_resource_group" "test_rg" {
   location = "West Europe"
 } 
 
+resource "azurerm_virtual_network" "test_vnet" {
+  name                = "vnet03"
+  location            = azurerm_resource_group.test_rg.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+  address_space       = ["10.4.0.0/16"]
+}
+
+# Subnets for App Service instances
+resource "azurerm_subnet" "test_sub1" {
+  name                 = "subnet01"
+  resource_group_name  = azurerm_resource_group.test_rg.name
+  virtual_network_name = azurerm_virtual_network.test_vnet.name
+  address_prefixes     = ["10.4.1.0/24"]
+  enforce_private_link_endpoint_network_policies = true
+  }
+
+
 resource "azurerm_app_service_plan" "app_service_plan" {
   name                = "royal01"
   location            = azurerm_resource_group.test_rg.location
@@ -39,4 +56,55 @@ resource "azurerm_app_service" "app_service" {
       "SOME_KEY" = "some-value"
   }*/
 
+}
+
+#private endpoint
+
+resource "azurerm_private_endpoint" "pe1" {
+  name                = "${azurerm_app_service.app_service.name}-endpoint"
+  location            = azurerm_resource_group.test_rg.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+  subnet_id           = azurerm_subnet.test_sub1.id
+  
+
+  private_service_connection {
+    name                           = "${azurerm_app_service.app_service.name}-privateconnection"
+    private_connection_resource_id = azurerm_app_service.app_service.id
+    subresource_names = ["sites"]
+    is_manual_connection = false
+  }
+}
+
+# private DNS
+resource "azurerm_private_dns_zone" "dns1" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = azurerm_resource_group.test_rg.name
+}
+
+#private DNS Link
+resource "azurerm_private_dns_zone_virtual_network_link" "dnszone1" {
+  name                  = "${azurerm_app_service.app_service.name}-dnslink"
+  resource_group_name   = azurerm_resource_group.test_rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns1.name
+  virtual_network_id    = azurerm_virtual_network.test_vnet.id
+  registration_enabled = false
+}
+
+resource "azurerm_subnet" "test_sub2" {
+  name                 = "subnet02"
+  resource_group_name  = azurerm_resource_group.test_rg.name
+  virtual_network_name = azurerm_virtual_network.test_vnet.name
+  address_prefixes     = ["10.4.2.0/27"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+        actions = [
+            "Microsoft.Network/virtualNetworks/subnets/action",
+            "Microsoft.Network/virtualNetworks/subnets/join/action"
+          ]
+        name    = "Microsoft.Web/serverFarms"
+      }
+  }
 }
